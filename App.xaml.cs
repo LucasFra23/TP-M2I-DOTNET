ca0c20be26c90c78fe6a0f37561560c47464f7d3
Lucas <lucas.francois2301@gmail.com>
2025-05-16 09:48:00 +0200

    Add sample project

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TP_M2I_DOTNET
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TP_M2I_DOTNET
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
